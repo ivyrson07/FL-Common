a1b3e0905503cddefed93a628b7ac1842154f4cc
Ivyrson Garcia <ivyrsongarcia17@gmail.com>
2023-09-20 05:17:37 +0800

    Added routing keys constants.

---
+++ FL.Common/FL.Common/Models/Catalog.cs
@@ -0,0 +1,13 @@
+﻿namespace FL.Common.Models
+{
+    public class Catalog
+    {
+        public Guid Id { get; set; }
+
+        public string? Code { get; set; }
+
+        public string? Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ FL.Common/FL.Common/RoutingKeys/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FL.Common.RoutingKeys
+{
+    public partial class Topic
+    {
+        public const string TestCommentAny = "test.*";
+        public const string TestCommentPrivate = "test.privatecomment";
+        public const string TestCommentPublic = "test.publiccomment";
+    }
+}
